019462bfd59da0f1339dc4393f74de3fba8f49d7
Aditya Mishra <mishraaaditya29@gmail.com>
2024-08-14 14:47:38 +0530

    example3 added

---
+++ Lecture-2/myshell3.sh
@@ -0,0 +1,12 @@
+# checking if a package is installed or not already
+# if not installed, then install it
+
+command=/usr/bin/top
+
+if [ -f $command]
+then
+    echo "the $command command already exists"
+else
+    echo "$command command does not exist. installing it right now!!!"
+    sudo apt update && sudo apt install -y htop
+fi
